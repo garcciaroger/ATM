90c818b648ba400b7ab11026aff4469ebb7a3d36
Roger Garcia <garcciaroger@gmail.com>
2024-02-08 14:00:06 -0600

    Modified functions

--- Account/account.cpp
+++ Account/account.cpp
@@ -5,36 +5,34 @@
 //This deposit function allows a deposit amount and returns 
 //weather or not transfer was a success with balance update
 bool Account::deposit(double amount){
-    //Throws error if deposit amount is less than 0
-    if(amount < 0){
+    // Throws error if deposit amount is less than 0
+    if (amount < 0) {
         throw std::invalid_argument("Balance cannot be negative\n");
     }
-    //checks to see if there are numbers only present
-    std::string amount_str = std::to_string(amount);
-    for(char c: amount_str){
-        if(!std::isdigit(c)){
-            throw std::invalid_argument("Invalid Character detected\n");
-        }
+    // Check if the double value is effectively an integer
+    if (static_cast<int>(amount) != amount) {
+        throw std::invalid_argument("Invalid character detected\n");
     }
-    //Returns true and updates balance
+    // Returns true and updates balance
     balance += amount;
     return true;
 }
 //This deposit function allows a withdraw amount and returns 
 //weather or not transfer was a success with balance update
 bool Account::withdraw(double amount){
-    //Throws error if amount is greater than balance
-    if(amount > balance){
+    // Throws error if amount is greater than balance
+    if (amount > balance) {
         throw std::invalid_argument("Withdraw amount greater than balance\n");
+        return false;
     }
-    //checks to see if there are numbers only present
-    std::string amount_str = std::to_string(amount);
-    for(char c: amount_str){
-        if(!std::isdigit(c)){
-            throw std::invalid_argument("Invalid Character detected\n");
-        }
+
+    // Check if the double value is effectively an integer
+    if (static_cast<int>(amount) != amount) {
+        throw std::invalid_argument("Invalid character detected\n");
+        return false;
     }
-    //Returns true and updates balance
+
+    // Returns true and updates balance
     balance -= amount;
     return true;
 }
