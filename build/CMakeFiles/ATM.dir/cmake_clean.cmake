a7382732d25f7c2e65f890cb82ee58f70dbdb7f5
Roger Garcia <garcciaroger@gmail.com>
2024-02-08 13:58:10 -0600

    Created CMAKE File

---
+++ build/CMakeFiles/ATM.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "ATM"
+  "ATM.pdb"
+  "CMakeFiles/ATM.dir/Account/account.cpp.o"
+  "CMakeFiles/ATM.dir/Account/account.cpp.o.d"
+  "CMakeFiles/ATM.dir/main.cpp.o"
+  "CMakeFiles/ATM.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ATM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
