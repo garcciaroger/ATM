99738a9bac730a06e63d00ebcec8dbf2b9578f07
Roger Garcia <garcciaroger@gmail.com>
2024-02-08 20:42:26 -0600

    test

---
+++ Account/build/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/rogergarcia/ATM/Account/googletest/googletest
+# Build directory: /Users/rogergarcia/ATM/Account/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Account/build/test_executable[1]_tests.cmake
@@ -0,0 +1,3 @@
+add_test([=[AccountTest.DepositTest]=]  /Users/rogergarcia/ATM/Account/build/test_executable [==[--gtest_filter=AccountTest.DepositTest]==] --gtest_also_run_disabled_tests)
+set_tests_properties([=[AccountTest.DepositTest]=]  PROPERTIES WORKING_DIRECTORY /Users/rogergarcia/ATM/Account/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set(  test_executable_TESTS AccountTest.DepositTest)
